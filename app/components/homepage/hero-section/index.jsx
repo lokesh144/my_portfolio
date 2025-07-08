f5fb175ccae3e68a4cd0b7fc246b1ad49bcd6410
lokesh144 <lokeshpan31@gmail.com>
2025-07-08 23:33:38 +0545

    toml added

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -129,7 +129,7 @@ function HeroSection() {
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">MongoDB</span>
                 <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">Docker'</span>
+                <span className="text-amber-300">Docker{","}</span>
                 {/* <span className="text-gray-400">{"', '"}</span> */}
                 {/* <span className="text-amber-300">AWS</span>
                 <span className="text-gray-400">{"'],"}</span> */}
