b2a8bfdeda8d7d7fec92ed8e8ea1283dd4731279
lokesh144 <lokeshpan31@gmail.com>
2025-07-09 09:08:48 +0545

    SSR solved3

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,7 +1,7 @@
 // @flow strict
 
-import { personalData } from "@/utils/data/personal-data";
-import BlogCard from "../components/homepage/blog/blog-card";
+// import { personalData } from "@/utils/data/personal-data";
+// import BlogCard from "../components/homepage/blog/blog-card";
 
 // async function getBlogs() {
 //   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
@@ -14,31 +14,31 @@ import BlogCard from "../components/homepage/blog/blog-card";
 //   return data;
 // };
 
-async function page() {
-  // const blogs = await getBlogs();
+// async function page() {
+//   const blogs = await getBlogs();
 
-  return (
-    <div className="py-8">
-      <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
-            All Blog
-          </span>
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-        </div>
-      </div>
+//   return (
+//     <div className="py-8">
+//       <div className="flex justify-center my-5 lg:py-8">
+//         <div className="flex  items-center">
+//           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+//           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
+//             All Blog
+//           </span>
+//           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+//         </div>
+//       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
-        {
-          blogs.map((blog, i) => (
-            blog?.cover_image &&
-            <BlogCard blog={blog} key={i} />
-          ))
-        }
-      </div>
-    </div>
-  );
-};
+//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
+//         {
+//           blogs.map((blog, i) => (
+//             blog?.cover_image &&
+//             <BlogCard blog={blog} key={i} />
+//           ))
+//         }
+//       </div>
+//     </div>
+//   );
+// };
 
-export default page;
+// export default page;
