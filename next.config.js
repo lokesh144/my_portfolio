8868269aa30703a7f5efa67f9db6f2c569ebc8a0
lokesh144 <lokeshpan31@gmail.com>
2026-08-31 22:23:49 +0545

    Enable Next.js server routes

--- next.config.js
+++ next.config.js
@@ -1,28 +1,29 @@
-const path = require('path')
- 
+const path = require("path");
+
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
+        protocol: "https",
+        hostname: "media.dev.to",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'media2.dev.to',
-        pathname: '**',
+        protocol: "https",
+        hostname: "media2.dev.to",
+        pathname: "**",
       },
     ],
-  unoptimized: true,
+
+    unoptimized: true,
   },
-  output: 'export',
-}
+};
